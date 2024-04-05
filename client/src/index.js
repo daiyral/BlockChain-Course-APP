113cf6825e21ae108b0082774b8f481999e72e7a
Daniel Aibinder <dannyaibinder@gmail.com>
2024-04-05 11:01:18 +0300

    Updates

--- client/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { createRoot } from 'react-dom/client';
-import { FluentProvider, teamsLightTheme } from '@fluentui/react-components';
-const root = createRoot(document.getElementById("root"));
-root.render(<FluentProvider theme={teamsLightTheme}><App /> </FluentProvider>);
