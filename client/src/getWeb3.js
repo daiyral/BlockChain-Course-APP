bc6b1087b9f9ece064b15166c2f1c6b6ba319d2b
Daniel Aibinder <dannyaibinder@gmail.com>
2024-04-05 10:55:14 +0300

    updates

--- client/src/getWeb3.js
+++ client/src/getWeb3.js
@@ -1,5 +1,4 @@
 import Web3 from "web3";
-
 const getWeb3 = () =>
   new Promise((resolve, reject) => {
     // Wait for loading completion to avoid race conditions with web3 injection timing.
@@ -25,9 +24,18 @@ const getWeb3 = () =>
       }
       // Fallback to localhost; use dev console port by default...
       else {
-        const provider = new Web3.providers.HttpProvider(
-          "http://127.0.0.1:8545"
-        );
+        let provider;
+        if (process.env.NODE_ENV === "production") {
+            provider = new Web3.providers.HttpProvider(
+              process.env.API_KEY
+          );
+        }
+        else{
+          provider = new Web3.providers.HttpProvider(
+            "http://localhost:8545"
+          );
+        }
+        
         const web3 = new Web3(provider);
         console.log("No web3 instance injected, using Local web3.");
         resolve(web3);
--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,6 @@
 const path = require("path");
-
+require('dotenv').config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -13,5 +14,19 @@ module.exports = {
       gas: 6721975,
       gasPrice: 20000000000,
     },
+    sepolia: {
+      provider: () => new HDWalletProvider({
+        mnemonic:{
+          phrase:process.env.MNEMONIC
+        },
+        providerOrUrl:process.env.API_KEY
+      }),
+      network_id: 11155111, // Sepolia's network ID
+      gas: 6721975, // Adjust the gas limit as per your requirements
+      gasPrice: 20000000000, // Set the gas price to an appropriate value
+      confirmations: 2, // Set the number of confirmations needed for a transaction
+      timeoutBlocks: 200, // Set the timeout for transactions
+      skipDryRun: true // Skip the dry run option
+     }
   },
 };
